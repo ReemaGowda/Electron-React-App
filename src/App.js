5a42f8bcc2d80004a15a84ef5d919108129ec3b6
Reema Gowda <37684418+ReemaGowda@users.noreply.github.com>
2020-08-28 10:59:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const electron = window.require("electron");
+const ipcRenderer = electron.ipcRenderer;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://www.reddit.com/r/aww.json")
+      .then((response) => {
+        console.log(response.data.data.children);
+        this.setState({
+          posts: response.data.data.children,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  showImage = () => {
+    ipcRenderer.send("toggle-image");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ul class="list-group list-group-flush">
+          {this.state.posts.map((post) => (
+            <li
+              key={post.data.id}
+              className="list-group-item flex-container"
+              onClick={() => this.showImage()}
+            >
+              <img
+                src={post.data.thumbnail}
+                alt="thumbnail"
+                className="thumbnail"
+              />
+              <div>{post.data.title}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
+
